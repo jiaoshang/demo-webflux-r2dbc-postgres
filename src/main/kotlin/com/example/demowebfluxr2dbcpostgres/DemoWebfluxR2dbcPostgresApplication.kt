35e65c7861f06abe5bdbb2fd3f4495ef6a3e3aea
Chongze Jiao <cjiao@bol.com>
2019-08-14 16:20:59 +0200

    first commit

---
+++ src/main/kotlin/com/example/demowebfluxr2dbcpostgres/DemoWebfluxR2dbcPostgresApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demowebfluxr2dbcpostgres
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoWebfluxR2dbcPostgresApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoWebfluxR2dbcPostgresApplication>(*args)
+}
